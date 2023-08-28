3b1733bba6d2420a131277f39b1b6b84395ffaba
Marlon da Cunha <marloncunhaofficial@gmail.com>
2023-08-28 12:48:51 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Paragrafo from "../../components/Paragrafo"
 import Titulo from "../../components/Title"
+import Avatar from "../../components/avatar"
+
+import { Descrisao, BotaoTema, SideBarContainer } from "./style"
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/marloncunhabr.png" />
-    <Titulo fontSize={20}>Marlon da Cunha</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={18}>Marlon Cunha</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        marloncunhaof
+      </Paragrafo>
+      <Descrisao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descrisao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
