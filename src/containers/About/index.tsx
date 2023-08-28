8cc85a74a911cc6596fb64d8dd3a528e7e2df266
Marlon da Cunha <marloncunhaofficial@gmail.com>
2023-08-28 09:47:29 -0300

    construindo a estrutura principal e a seção sobre

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from "../../components/Title"
 import Paragrafo from "../../components/Paragrafo"
+import { GithubSecao } from "./styles"
 
 const Sobre = () => (
   <section>
@@ -8,6 +9,10 @@ const Sobre = () => (
       Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto nulla
       fugiat dignissimos distinctio dolorum commodi repudiandae iste velit eum.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=marloncunhabr&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=marloncunhabr&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
